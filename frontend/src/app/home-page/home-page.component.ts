503d97548a1530591b21324d96533ebd8cdd6362
AshishShimpi <ashishshimpi99@gmail.com>
2022-10-01 14:39:11 +0530

    added map

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -9,6 +9,36 @@ export class HomePageComponent implements OnInit {
 
     constructor() { }
 
+    carList:any[]=[{
+        thumbnail:"../../assets/UberBlack.jpg",
+        name : "UberBlack",
+    },
+    {
+        thumbnail:"../../assets/UberBlackSUV.jpg",
+        name : "UberBlackSUV",
+    },
+    {
+        thumbnail:"../../assets/UberComfort.jpg",
+        name : "UberComfort",
+    },
+    {
+        thumbnail:"../../assets/UberGreen.jpg",
+        name : "UberGreen",
+    },
+    {
+        thumbnail:"../../assets/UberSelect.jpeg",
+        name : "UberSelect",
+    },
+    {
+        thumbnail:"../../assets/UberXL.jpg",
+        name : "UberXL",
+    },
+    {
+        thumbnail:"../../assets/UberBlack.jpg",
+        name : "UberBlack",
+    },
+    ];
+
     ngOnInit(): void {
     }
 
--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import maplibregl from "maplibre-gl";
+import MapLibreGlDirections, { LoadingIndicatorControl, layersFactory } from "@maplibre/maplibre-gl-directions";
+import { layers } from 'src/assets/restyle';
 
 @Component({
     selector: 'app-map',
@@ -9,7 +11,6 @@ import maplibregl from "maplibre-gl";
 export class MapComponent implements OnInit {
 
     map: any;
-    count: number = 9;
     constructor() { }
 
     ngOnInit(): void {
@@ -20,19 +21,57 @@ export class MapComponent implements OnInit {
         this.map = new maplibregl.Map({
             container: 'map',
             style: 'https://api.maptiler.com/maps/bright/style.json?key=NsTE7pRDxbgmuDt7bEY8',
-            // stylesheet location
             center: [72.99984769174932, 19.12596157299783], // starting position [lng, lat]
             zoom: 12 // starting zoom,
         });
+
         let nav = new maplibregl.NavigationControl({});
         this.map.addControl(nav, 'top-right');
-        this.map.addControl(new maplibregl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true
-            },
-        }));
+        const lay = layersFactory();
+        console.log(lay);
+
+        this.map.loadImage("../../assets/marker-icon.png", (error, image) => {
+            if (!error) this.map.addImage("balloon-waypoint", image);
+        });
+
+        this.map.loadImage("../../assets/direction-arrow.png", (error, image) => {
+            if (!error && image) {
+                this.map.addImage("direction-arrow", image);
+            }
+        });
+
+        
+
+        this.map.on("load", () => {
+
+            const directions = new MapLibreGlDirections(this.map, {
+                profile: "driving",
+                requestOptions: {
+                    alternatives: "true",
+                    annotations: "distance,duration"
+                },
+                layers,
+                sensitiveWaypointLayers: ["maplibre-gl-directions-waypoint", "maplibre-gl-directions-waypoint-casing"],
+                sensitiveSnappointLayers: ["maplibre-gl-directions-snappline"],
+                sensitiveRoutelineLayers: ["maplibre-gl-directions-routeline"],
+                sensitiveAltRoutelineLayers: ["maplibre-gl-directions-alt-routeline"],
+            });
+            this.map.addControl(new LoadingIndicatorControl(directions));
+            // directions.interactive = true;
+            directions.setWaypoints([
+                [73.2753736386154, 19.11822160710978],
+                [72.99984769174932, 19.12596157299783]
+            ]);
+            this.map.fitBounds([[73.2753736386154, 19.11822160710978],
+                [72.99984769174932, 19.12596157299783]], {
+                    padding: { top: 150, bottom: 100, left: 100, right: 100 },
+                    linear : false,
+                    duration: 1000,
+                    pitch : 30
+                });
+        });
+
     }
-    // Control implemented as ES6 class
 
 }
 
